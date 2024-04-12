68e431d5ac121d7691352caebf006b89fa8c7f33
xiongcaihu <xiongcaihu@qq.com>
2024-04-12 04:29:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/store/global/slices/settings/initialState.ts
@@ -0,0 +1,22 @@
+import { DeepPartial } from 'utility-types';
+
+import { DEFAULT_SETTINGS } from '@/const/settings';
+import { GlobalServerConfig } from '@/types/serverConfig';
+import { GlobalSettings } from '@/types/settings';
+
+export interface GlobalSettingsState {
+  avatar?: string;
+  defaultSettings: GlobalSettings;
+  editingCustomCardModel?: { id: string; provider: string } | undefined;
+  serverConfig: GlobalServerConfig;
+  settings: DeepPartial<GlobalSettings>;
+  userId?: string;
+}
+
+export const initialSettingsState: GlobalSettingsState = {
+  defaultSettings: DEFAULT_SETTINGS,
+  serverConfig: {
+    telemetry: {},
+  },
+  settings: {},
+};
